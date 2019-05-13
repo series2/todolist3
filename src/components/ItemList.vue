676fdd008eb581c3a86c6dcf8c272a32acff7389
series2 <series2.others@gmail.com>
2019-05-14 01:28:40 +0900

    商品リスト追加

--- src/components/ItemList.vue
+++ src/components/ItemList.vue
@@ -1,10 +1,26 @@
 <template>
-  <div>ItemList</div>
+  <div>
+    <div>ItemList</div>
+    <div v-for="item in items" :key="item.name">
+      <div class="item">
+        <div class="name">名前: {{ item.name }}</div>
+        <div class="price">{{ item.price }} 円</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "ItemList"
+  name: "ItemList",
+  data() {
+    return {
+      items: [
+        {name: 'たまご', price: 100},
+        {name: 'りんご', price: 160}
+      ]
+    }
+  }
 };
 </script>
 
